0c1a42b2a0e35e6a1916d21d3d6098e1bc7c9156
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-15 13:01:59 +0200

    simpleLightbox

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
