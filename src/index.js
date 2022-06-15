5571c2396bbbcc4e3c69ad383069fd90cdb6b76a
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-15 14:02:49 +0200

    html tags

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+
+// Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
