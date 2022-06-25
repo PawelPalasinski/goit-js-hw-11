da70f9b95d6bdf17fd04a9314bbf478ff0784e22
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-25 07:50:45 +0200

    fetchDataAsync - done

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 import simpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { fetchData } from './js/fetchData';
+import { fetchDataAsync } from './js/fetchData';
 
 const getBtn = document.querySelector(".search-form>button");
+const form = document.querySelector('.search-form__input-container');
 
 // const getData = () => {
 //     axios.get(
@@ -27,19 +28,20 @@ const getBtn = document.querySelector(".search-form>button");
 
 
 qSelected = document.querySelector(".search-form__input-container>input");
+console.log(qSelected.value.trim());
 
 const searchData = e => {
     const searchTerm = qSelected.value.trim();
 
-    fetchData(searchTerm)
-        .then(data => {
-            dataFound(data);
-        })
-        .catch(error => {
-            if (searchTerm !== '') {
-                console.log('no data to display')
-            }
-        })
+    fetchDataAsync(searchTerm)
+      .then(data => {
+        dataFound(data);
+      })
+      .catch(error => {
+        if (searchTerm !== '') {
+          console.log('no data to display');
+        }
+      });
     e.preventDefault();
 };
 
@@ -48,4 +50,12 @@ const searchData = e => {
 //     return console.log(dataToDisplay);
 // }
 
-qSelected.addEventListener('input', searchData);
+
+function pixebayData(data) {
+    return (searchData.innerHTML = data.map(
+        item => `<div> ${largeImageURL} </div>`
+    ).join(''));
+}
+
+
+form.addEventListener('submit', searchData);
--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,34 +1,16 @@
 import axios from 'axios';
-const axiosRequest = require('axios');
 
-
-// export function fetchData(searchTerm) {
-//   const baseURL = 'https://pixabay.com/api/';
-//   const key = '28143013-44919de38ad9e5402793063fb';
-//   axiosRequest
-//     .get(`${baseURL}?key=${key}&q=${searchTerm}`)
-//     .then(response => {
-//       return response;
-//     })
-//     .catch(error => {
-//       `ERRPR ${error}`;
-//     });
-// }
-
-// fetchDataAsync('cat');
-
-async function fetchDataAsync(searchTerm) {
+export async function fetchDataAsync(searchTerm) {
   try {
     const baseURL = 'https://pixabay.com/api/';
     const key = '28143013-44919de38ad9e5402793063fb';
-    let response = await axiosRequest.get(
-      `${baseURL}?key=${key}&q=${searchTerm}`
+    const perPage = 40;
+    const page = 1;
+    let response = await axios.get(
+      `${baseURL}?key=${key}&q=${searchTerm}&image_type="photo"&per_page=${perPage}&page=${page}`
     );
-    return response;
+    return response.data;
   } catch (error) {
     console.error(`ERROR: ${error}`);
   }
 }
-
-
-fetchDataAsync('cat');
