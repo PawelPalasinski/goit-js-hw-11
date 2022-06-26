b3bbb2583010be9bfce47dd49ae63589fa207129
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-26 15:07:58 +0200

    js done

--- src/index.js
+++ src/index.js
@@ -1,61 +1,159 @@
+// Imports: SimpleLightbox, Notiflix & Axios
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
 import axios from 'axios';
 
-import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
+// HTML elements
 
-import simpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+const searchingBox = document.querySelector('.searching-box');
+const searchQuery = document.querySelector('input[name="searchQuery"]');
+const upBtn = document.querySelector('.up-btn');
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
-import { fetchDataAsync } from './js/fetchData';
+// Needed to query the Pixabay API
 
-const getBtn = document.querySelector(".search-form>button");
-const form = document.querySelector('.search-form__input-container');
+const baseURL = 'https://pixabay.com/api/';
+const key = '28143013-44919de38ad9e5402793063fb';
+let perPage = 40;
+let page = 0;
+let name = searchQuery.value;
 
-// const getData = () => {
-//     axios.get(
-//         `https://pixabay.com/api/?key=28143013-44919de38ad9e5402793063fb`
-//     ).then(response => {
-//         console.log(response);
-//     })
-//     .catch((error) => console.log(error));
-// };
+// Needed to hide "load more" and "arrow up" buttons
 
-// getBtn.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     getData();
-// });
+loadBtn.style.display = 'none';
+upBtn.style.display = 'none';
 
+// Fetch images from Pixabay API using Axios
 
-
-qSelected = document.querySelector(".search-form__input-container>input");
-console.log(qSelected.value.trim());
-
-const searchData = e => {
-    const searchTerm = qSelected.value.trim();
-
-    fetchDataAsync(searchTerm)
-      .then(data => {
-        dataFound(data);
-      })
-      .catch(error => {
-        if (searchTerm !== '') {
-          console.log('no data to display');
-        }
-      });
-    e.preventDefault();
-};
-
-// function dataFound(data) {
-//     const dataToDisplay = data.map({webformatURL} => `adres obrazka: ${webformatURL}`);
-//     return console.log(dataToDisplay);
-// }
-
-
-function pixebayData(data) {
-    return (searchData.innerHTML = data.map(
-        item => `<div> ${largeImageURL} </div>`
-    ).join(''));
+async function fetchImages(name, page) {
+  try {
+    const response = await axios.get(
+      `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
 }
 
+// Handling the "submit" button event
 
-form.addEventListener('submit', searchData);
+async function eventHandler(e) {
+  e.preventDefault();
+  gallery.innerHTML = '';
+  loadBtn.style.display = 'none';
+
+  page = 1;
+  name = searchQuery.value;
+
+  fetchImages(name, page)
+    .then(name => {
+      let totalPages = name.totalHits / perPage;
+
+      if (name.hits.length > 0) {
+        Notiflix.Notify.success(`Hooray! We found ${name.totalHits} images.`);
+        renderGallery(name);
+        new SimpleLightbox('.gallery a');
+        upBtn.style.display = 'block';
+        upBtn.addEventListener('click', () => {
+          searchingBox.scrollIntoView({
+            behavior: 'smooth',
+          });
+        });
+
+        if (page < totalPages) {
+          loadBtn.style.display = 'block';
+        } else {
+          loadBtn.style.display = 'none';
+          Notiflix.Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        gallery.innerHTML = '';
+      }
+    })
+    .catch(error => console.log('ERROR: ' + error));
+}
+
+searchForm.addEventListener('submit', eventHandler);
+
+// Funktion for markup for HTML gallery element
+
+function renderGallery(name) {
+  const markup = name.hits
+    .map(hit => {
+      return `<div class="photo-card">
+
+        <a class="gallery-item" href="${hit.largeImageURL}">
+          <img
+            class="gallery__image"
+            src="${hit.webformatURL}"
+            alt="${hit.tags}"
+            loading="lazy"
+        /></a>
+
+        <div class="info">
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">thumb_up</b>
+            </p>
+            <p class="info-counter">${hit.likes}</p>
+          </div>
+
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">visibility</b>
+            </p>
+            <p class="info-counter">${hit.views}</p>
+          </div>
+
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">forum</b>
+            </p>
+            <p class="info-counter">${hit.comments}</p>
+          </div>
+
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">download</b>
+            </p>
+            <p class="info-counter">${hit.downloads}</p>
+          </div>
+
+        </div>
+      </div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// Load more button - function 
+
+loadBtn.addEventListener(
+  'click',
+  () => {
+    name = searchQuery.value;
+    page += 1;
+    fetchImages(name, page).then(name => {
+      let totalPages = name.totalHits / perPage;
+      renderGallery(name);
+      new SimpleLightbox('.gallery a');
+      if (page >= totalPages) {
+        loadBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    });
+  },
+  true
+);
